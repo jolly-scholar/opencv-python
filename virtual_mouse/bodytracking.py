0cab027368a2668e53627c556b7cf3af7201075c
_lane <josephyounglee@gmail.com>
2024-03-21 23:33:06 -0700

    body tracker init

---
+++ virtual_mouse/bodytracking.py
@@ -0,0 +1,8 @@
+import cv2
+import time
+import mediapipe as mp
+import HandTrackingModule as htm
+
+
+#This program will perform full body tracking using the mediapipe/openpose model along with open cv.
+
